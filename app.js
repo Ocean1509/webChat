08c72ffa00e142b8f83cfaa9103c506a56e18931
Ocean1509 <593704361@qq.com>
2016-10-27 10:46:58 +0800

    初始功能实现

---
+++ app.js
@@ -0,0 +1,52 @@
+const http=require('http');
+const express=require('express');
+const app=express();
+const path=require('path');
+const server=http.createServer(app);
+const fs=require('fs');
+const io=require('socket.io').listen(server);
+console.log(__dirname);
+console.log(path.join(__dirname,'static'))
+app.use(express.static(path.join(__dirname,'static')))
+server.listen(8080)
+//设置日志级别
+io.set('log level', 1); 
+app.get('/',(req,res)=>{
+	res.sendfile(__dirname+'/index.html');
+});
+
+//WebSocket连接监听
+io.on('connection', function (socket) {
+	var client={
+		// socket:socket,
+		name:false,
+		text:'',
+		time:''
+	}
+  	socket.emit('connect');
+	socket.on('message',(msg)=>{
+		console.log(msg)
+		if(!client.name){	
+			client.name=msg;
+			socket.emit('login',client.name);
+			socket.broadcast.emit('otherlogin',{name:client.name,time:getTime()})
+		}else{
+			client.text=msg;
+			client.time=getTime();
+			socket.emit('emitMes',client);
+			socket.broadcast.emit('notice',client)
+
+		}
+	});
+	socket.on('disconnect',()=>{
+		console.log('--------------')
+		socket.broadcast.emit('loginout',{name:client.name,time:getTime()})
+	});
+});
+
+
+
+const getTime=()=>{
+	var date=new Date();
+	return date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+}
---
+++ static/chat.js
@@ -0,0 +1,41 @@
+$(function(){
+	var input=$('#input')
+	var socket=io.connect('http://localhost:8080');
+	socket.on('connect',()=>{
+		console.log('-----------')
+		$('#content').append('<li>连接中...</li>')
+	})
+
+	socket.on('login',function(data){
+		var content='<li>'+data+',欢迎您</li>'
+		$('#content').append(content)
+	});
+	socket.on('emitMes',function(data){
+		var content='<li>'+data.time+" "+'我说: '+data.text;
+		$('#content').append(content)
+	});
+	socket.on('otherlogin',function(data){
+		var content='<li>'+data.time+" "+data.name+'进入聊天</li>';
+		$('#content').append(content)
+	});
+	socket.on('notice',function(data){
+		var content='<li>'+data.time+" "+data.name+'说: '+data.text;
+		$('#content').append(content)
+
+	});
+	socket.on('loginout',function(data){
+		console.log(data)
+		var content='<li>'+data.time+" "+data.name+'退出聊天</li>';
+		console.log('--------------')
+		$('#content').append(content)
+	});
+	input.keypress(function(e) {
+		/* Act on the event */
+		if(e.keyCode=='13'){
+			var value=$('#input').val()
+			socket.emit('message',value);
+			$('#input').val('')
+		}
+	});
+})
+

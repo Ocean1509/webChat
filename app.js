eb88fd19a6a44f53e16894a40859e0b3e7c442d8
Ocean1509 <593704361@qq.com>
2016-10-28 13:58:08 +0800

    aaa

--- app.js
+++ app.js
@@ -16,33 +16,33 @@ app.get('/',(req,res)=>{
 });
 
 //WebSocket连接监听
-io.on('connection', function (socket) {
-	var client={
-		// socket:socket,
-		name:false,
-		text:'',
-		time:''
-	}
-  	socket.emit('connect');
-	socket.on('message',(msg)=>{
-		console.log(msg)
-		if(!client.name){	
-			client.name=msg;
-			socket.emit('login',client.name);
-			socket.broadcast.emit('otherlogin',{name:client.name,time:getTime()})
-		}else{
-			client.text=msg;
-			client.time=getTime();
-			socket.emit('emitMes',client);
-			socket.broadcast.emit('notice',client)
+// io.on('connection', function (socket) {
+// 	var client={
+// 		// socket:socket,
+// 		name:false,
+// 		text:'',
+// 		time:''
+// 	}
+//   	socket.emit('connect');
+// 	socket.on('message',(msg)=>{
+// 		console.log(msg)
+// 		if(!client.name){	
+// 			client.name=msg;
+// 			socket.emit('login',client.name);
+// 			socket.broadcast.emit('otherlogin',{name:client.name,time:getTime()})
+// 		}else{
+// 			client.text=msg;
+// 			client.time=getTime();
+// 			socket.emit('emitMes',client);
+// 			socket.broadcast.emit('notice',client)
 
-		}
-	});
-	socket.on('disconnect',()=>{
-		console.log('--------------')
-		socket.broadcast.emit('loginout',{name:client.name,time:getTime()})
-	});
-});
+// 		}
+// 	});
+// 	socket.on('disconnect',()=>{
+// 		console.log('--------------')
+// 		socket.broadcast.emit('loginout',{name:client.name,time:getTime()})
+// 	});
+// });
 
 
 
--- static/chat.js
+++ static/chat.js
@@ -1,40 +1,40 @@
 $(function(){
 	var input=$('#input')
-	console.log(window.location)
-	var socket=io.connect(window.location.hostname);
-	socket.on('connect',()=>{
-		console.log('-----------')
-		$('#content').append('<li>连接中...</li>')
-	})
+	console.log(window.location.hostname)
+	// var socket=io.connect(window.location.hostname);
+	// socket.on('connect',()=>{
+	// 	console.log('-----------')
+	// 	$('#content').append('<li>连接中...</li>')
+	// })
 
-	socket.on('login',function(data){
-		var content='<li>'+data+',欢迎您</li>'
-		$('#content').append(content)
-	});
-	socket.on('emitMes',function(data){
-		var content='<li>'+data.time+" "+'我说: '+data.text;
-		$('#content').append(content)
-	});
-	socket.on('otherlogin',function(data){
-		var content='<li>'+data.time+" "+data.name+'进入聊天</li>';
-		$('#content').append(content)
-	});
-	socket.on('notice',function(data){
-		var content='<li>'+data.time+" "+data.name+'说: '+data.text;
-		$('#content').append(content)
+	// socket.on('login',function(data){
+	// 	var content='<li>'+data+',欢迎您</li>'
+	// 	$('#content').append(content)
+	// });
+	// socket.on('emitMes',function(data){
+	// 	var content='<li>'+data.time+" "+'我说: '+data.text;
+	// 	$('#content').append(content)
+	// });
+	// socket.on('otherlogin',function(data){
+	// 	var content='<li>'+data.time+" "+data.name+'进入聊天</li>';
+	// 	$('#content').append(content)
+	// });
+	// socket.on('notice',function(data){
+	// 	var content='<li>'+data.time+" "+data.name+'说: '+data.text;
+	// 	$('#content').append(content)
 
-	});
-	socket.on('loginout',function(data){
-		console.log(data)
-		var content='<li>'+data.time+" "+data.name+'退出聊天</li>';
-		console.log('--------------')
-		$('#content').append(content)
-	});
+	// });
+	// socket.on('loginout',function(data){
+	// 	console.log(data)
+	// 	var content='<li>'+data.time+" "+data.name+'退出聊天</li>';
+	// 	console.log('--------------')
+	// 	$('#content').append(content)
+	// });
 	input.keypress(function(e) {
 		/* Act on the event */
 		if(e.keyCode=='13'){
 			var value=$('#input').val()
-			socket.emit('message',value);
+			// socket.emit('message',value);
 			$('#input').val('')
 		}
 	});

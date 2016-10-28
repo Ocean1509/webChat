3015c3751d27e15e2a2684058fbe8182d45692d6
Ocean1509 <593704361@qq.com>
2016-10-28 14:16:51 +0800

    d

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-// const http=require('http');
-// const express=require('express');
-// const app=express();
-// const path=require('path');
-// const server=http.createServer(app);
-// const fs=require('fs');
-// //const io=require('socket.io').listen(server);
-// console.log(__dirname);
-// console.log(path.join(__dirname,'static'))
-// app.use(express.static(path.join(__dirname,'static')))
-// server.listen(process.env.PROT||8081);
-// //设置日志级别
-// //io.set('log level', 1); 
-// app.get('/',(req,res)=>{
-// 	res.sendfile(__dirname+'/index.html');
-// });
-
 const http=require('http');
-const url=require('url');
-const process=require('process');
-const path=require('path')
-var server=http.createServer(function(req,res){
-	res.writeHead(200,{'Content-Type': 'text/plain;charset=utf-8'});
-	res.end('发送成功')
+const express=require('express');
+const app=express();
+const path=require('path');
+const server=http.createServer(app);
+const fs=require('fs');
+//const io=require('socket.io').listen(server);
+console.log(__dirname);
+console.log(path.join(__dirname,'static'))
+app.use(express.static(path.join(__dirname,'static')))
+server.listen(process.env.PROT||8083);
+//设置日志级别
+//io.set('log level', 1); 
+app.get('/',(req,res)=>{
+	// res.sendfile(__dirname+'/index.html');
+	res.send('又一次成功')
 });
-server.listen(process.env.PORT||8083)
+
+// const http=require('http');
+// const url=require('url');
+// const process=require('process');
+// const path=require('path')
+// var server=http.createServer(function(req,res){
+// 	res.writeHead(200,{'Content-Type': 'text/plain;charset=utf-8'});
+// 	res.end('发送成功')
+// });
+// server.listen(process.env.PORT||8083)
 
 //WebSocket连接监听
 // io.on('connection', function (socket) {

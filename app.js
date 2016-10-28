f7f7874f59bd22ae9eb3cc038f7d57886dfa0f28
Ocean1509 <593704361@qq.com>
2016-10-28 15:09:10 +0800

    dd

--- app.js
+++ app.js
@@ -27,33 +27,33 @@ server.listen(process.env.PORT||8083)
 // app.listen(process.env.PORT||8083)
 
 //WebSocket连接监听
-// io.on('connection', function (socket) {
-// 	var client={
-// 		// socket:socket,
-// 		name:false,
-// 		text:'',
-// 		time:''
-// 	}
-//   	socket.emit('connect');
-// 	socket.on('message',(msg)=>{
-// 		console.log(msg)
-// 		if(!client.name){	
-// 			client.name=msg;
-// 			socket.emit('login',client.name);
-// 			socket.broadcast.emit('otherlogin',{name:client.name,time:getTime()})
-// 		}else{
-// 			client.text=msg;
-// 			client.time=getTime();
-// 			socket.emit('emitMes',client);
-// 			socket.broadcast.emit('notice',client)
+io.on('connection', function (socket) {
+	var client={
+		// socket:socket,
+		name:false,
+		text:'',
+		time:''
+	}
+  	socket.emit('connect');
+	socket.on('message',(msg)=>{
+		console.log(msg)
+		if(!client.name){	
+			client.name=msg;
+			socket.emit('login',client.name);
+			socket.broadcast.emit('otherlogin',{name:client.name,time:getTime()})
+		}else{
+			client.text=msg;
+			client.time=getTime();
+			socket.emit('emitMes',client);
+			socket.broadcast.emit('notice',client)
 
-// 		}
-// 	});
-// 	socket.on('disconnect',()=>{
-// 		console.log('--------------')
-// 		socket.broadcast.emit('loginout',{name:client.name,time:getTime()})
-// 	});
-// });
+		}
+	});
+	socket.on('disconnect',()=>{
+		console.log('--------------')
+		socket.broadcast.emit('loginout',{name:client.name,time:getTime()})
+	});
+});
 
 
 

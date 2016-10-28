9375ae94c81d698fab54b90f9136f2a04e24d074
Ocean1509 <593704361@qq.com>
2016-10-28 14:32:48 +0800

    ddd

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-const http=require('http');
-const express=require('express');
-const app=express();
-const path=require('path');
-const server=http.createServer(app);
-const fs=require('fs');
-//const io=require('socket.io').listen(server);
-console.log(__dirname);
-console.log(path.join(__dirname,'static'))
-app.use(express.static(path.join(__dirname,'static')))
-server.listen(process.env.PROT||8083);
-//设置日志级别
-//io.set('log level', 1); 
-app.get('/',(req,res)=>{
-	// res.sendfile(__dirname+'/index.html');
-	res.send('又一次成功')
-});
+// const http=require('http');
+// const express=require('express');
+// const app=express();
+// const path=require('path');
+// const server=http.createServer(app);
+// const fs=require('fs');
+// //const io=require('socket.io').listen(server);
+// console.log(__dirname);
+// console.log(path.join(__dirname,'static'))
+// app.use(express.static(path.join(__dirname,'static')))
+// server.listen(process.env.PROT||8083);
+// //设置日志级别
+// //io.set('log level', 1); 
+// app.get('/',(req,res)=>{
+// 	// res.sendfile(__dirname+'/index.html');
+// 	res.send('又一次')
+// });
 
 const http=require('http');
 const url=require('url');
-const express=require('express');
-const app=express()
 const process=require('process');
 const path=require('path')
 var server=http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type': 'text/plain;charset=utf-8'});
 	res.end('发送成功')
 });
-app.get('/',(req,res)=>{
-	res.send('成功')
-})
 server.listen(process.env.PORT||8083)
 
 //WebSocket连接监听
